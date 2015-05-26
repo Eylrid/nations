56feb154d340c6999d211236ac806e5341efc48d
Amos Bairn <Eylrid@Gmail.com>
2015-05-25 18:00:24 -0700

    Flip Call Direction
    Asker is in control and calls Quizer, instead of the other way around. This
    sets the stage for GUI based quizing.

--- nations.py
+++ nations.py
@@ -5,64 +5,92 @@ def load_list(listlocation = LISTLOCATION):
     with open(listlocation, 'r') as file:
         return [n for n in file.read().split('\n') if n]
 
-def quiz(nationlist, asker, tries=3, practice=True):
-    score = 0
-    starttime = time.time()
+def question_generator(nationlist):
     for nation in nationlist:
         othernations = nationlist[:]
         othernations.remove(nation)
         choices = random.sample(othernations, 3)
         choices.append(nation)
         random.shuffle(choices)
-        for i in range(tries):
-            answer = asker.ask(nation, choices)
-            if answer == nation:
-                score += tries - i
-                break
-            else:
-                feedback = ['Incorrect',]
-                if practice:
-                    feedback.append(nation)
-                asker.feedback(feedback)
+        yield nation, choices
 
-    timetaken = time.time() - starttime
+class Quizer:
+    def __init__(self, nationlist, tries=3, practice=True):
+        self.nationlist = nationlist
+        self.tries = tries
+        self.practice = practice
+        self.score = 0
+        self.questions = question_generator(nationlist)
 
-    asker.feedback((('Final score', score),
-                    ('Out of', len(nationlist)*tries),
-                    ('Time', timetaken),
-                    ('Points per second', score/timetaken)))
+    def start(self):
+        self.starttime = time.time()
+
+    def next(self):
+        self.attempts = 0
+        self.correctanswer, self.choices = self.questions.next()
+        return self.correctanswer, self.choices
+
+    def answer(self, answer):
+        if answer == self.correctanswer:
+            self.score += self.tries - self.attempts
+            return 'correct'
+        else:
+            self.attempts += 1
+            if self.attempts >= self.tries:
+                return 'move_on'
+            return 'try_again'
+
+    def end(self):
+        possible_score = len(self.nationlist)*self.tries
+        timetaken = time.time() - self.starttime
+        pointspersecond = self.score/timetaken
+        return self.score, possible_score, timetaken, pointspersecond
 
 class ConsoleAsker:
-    def ask(self, prompt, options):
-        print '-'*8
-        for i, option in enumerate(options):
-            print '%d) %s' %(i, option)
+    def __init__(self, *args, **kwargs):
+        self.quizer = Quizer(*args, **kwargs)
 
-        error_message = 'Pleas enter a number between 0 and %d.' %(len(options)-1)
+    def quiz(self):
+        self.quizer.start()
+        prompt, options = self.quizer.next()
         while True:
-            response = raw_input('? ')
-            if not response.isdigit():
-                print error_message
-                continue
-            n = int(response)
-            if n < 0 or n >= len(options):
-                print error_message
-                continue
-            return options[n]
+            print '-'*8
+            for i, option in enumerate(options):
+                print '%d) %s' %(i, option)
 
-    def feedback(self, feedback):
-        for item in feedback:
-            if isinstance(item, tuple) and len(item) == 2:
-                key, value = item
-                key, value = str(key), str(value)
-                print '%s: %s' %(key, value)
-            else:
-                print str(item)
+            error_message = 'Please enter a number between 0 and %d.' %(len(options)-1)
+            while True:
+                response = raw_input('? ')
+                if not response.isdigit():
+                    print error_message
+                    continue
+                n = int(response)
+                if n < 0 or n >= len(options):
+                    print error_message
+                    continue
+                answer = options[n]
+                result = self.quizer.answer(answer)
+                if result == 'try_again':
+                    print 'Incorrect,', prompt
+                else:
+                    break
+
+            try:
+                prompt, options = self.quizer.next()
+            except StopIteration:
+                break
+
+        score, possible_score, timetaken, pointspersecond = self.quizer.end()
+        print 'Score: %d' %score
+        print 'Out of: %d' %possible_score
+        print 'Time: %f' %timetaken
+        print 'Points per Second: %f' %pointspersecond
+
 
 def demo():
-    nationlist = load_list()
-    asker = ConsoleAsker()
-    quiz(nationlist, asker)
+    nationlist = load_list()[:4]
+    asker = ConsoleAsker(nationlist)
+    asker.quiz()
     raw_input('Press enter to quit')
 
 if __name__ == '__main__':
